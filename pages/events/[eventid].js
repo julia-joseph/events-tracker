8629b34b806d20da04b0d18bd6dad2e2e6c04735
Julia Joseph <juljoe726@gmail.com>
2021-07-27 18:49:56 +0530

    updating event item page

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,9 +1,30 @@
+import { useRouter } from "next/router";
+import EventItem from "../../components/events/event-item";
+import { getEventById } from "../../dummy-data";
+
 const EventPage = () => {
-    return (
-      <div>
-        <h1>Event</h1>
-      </div>
-    )
-  }
+  const router = useRouter();
+
+  const id = router.query.eventId;
+
+  const event = getEventById(id);
+
+  return (
+    <div>
+      {event &&
+        <EventItem
+            id={event.id}
+            title={event.title}
+            address={event.location}
+            date={event.date}
+            image={event.image}>
+        </EventItem>
+      }
+      {!event &&
+        <p>Event does not exist!!</p>
+      }
+    </div>
+  )
+}
   
 export default EventPage;
