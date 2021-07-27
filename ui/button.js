7ec93a26a35b454b128fc998603ec7e149cafbec
Julia Joseph <juljoe726@gmail.com>
2021-07-27 19:39:50 +0530

    button component refactor

--- ui/button.js
+++ ui/button.js
@@ -2,11 +2,15 @@ import Link from 'next/link';
 import classes from '../styles/button.module.css';
 
 const Button = props => {
-    return (
-        <Link href={props.link}>
-            <a className={classes.button}>{props.children}</a>
-        </Link>
-    )
+    if(props.link) {
+        return (
+            <Link href={props.link}>
+                <a className={classes.button}>{props.children}</a>
+            </Link>
+        )
+    }
+
+    return <button className={props.button} onClick={props.onClick}>{props.children}</button>
 }
 
 export default Button;
