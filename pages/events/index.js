677a554420490f8a54c19d78b6bccb81eb3e1cab
Julia Joseph <juljoe726@gmail.com>
2021-07-30 21:02:15 +0530

    SSG for events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,16 +1,26 @@
 import EventsList from "../../components/events/events-list";
 import EventsSearch from "../../components/events/events-search";
-import { getAllEvents } from "../../dummy-data";
+import { getEvents } from "../../utils/api-utils";
 
-const EventsPage = () => {
-  const events = getAllEvents();
+const EventsPage = props => {
   return (
     <>
       <EventsSearch></EventsSearch>
-      <EventsList items={events}></EventsList>
+      <EventsList items={props.events}></EventsList>
     </>
   )
-  
 };
+
+export async function getStaticProps() {
+  const events = await getEvents();
+  return {
+    props: {
+      events: events
+    },
+    revalidate: 60
+  }
+}
+
+
 
 export default EventsPage;
