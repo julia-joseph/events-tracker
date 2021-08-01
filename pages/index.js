749b6d450094ae49edb74f5ad4e979572b709749
julia-joseph <jujoe726@gmail.com>
2021-08-01 12:12:37 +0530

    adding title and meta date using Head component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
+import Head from "next/head";
 import EventsList from "../components/events/events-list";
 import { getFeaturedEvents } from "../utils/api-utils";
 
-
-const FeaturedEventsPage = props => {
-  return <EventsList items={props.featuredEvents}></EventsList>;
+const FeaturedEventsPage = (props) => {
+  return (
+    <>
+      <Head>
+        <title>Events Tracker</title>
+        <meta
+          name="description"
+          content="Find a bunch of events you've been waiting for"
+        />
+      </Head>
+      <EventsList items={props.featuredEvents}></EventsList>
+    </>
+  );
 };
 
 export async function getStaticProps() {
