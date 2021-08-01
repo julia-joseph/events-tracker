7541edc94ea781b6bb58cbab24965efd72be9ab9
julia-joseph <jujoe726@gmail.com>
2021-08-01 14:06:00 +0530

    adding headers for all pages (will be overridden)

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
+import Head from 'next/head';
+
 import Layout from "../layout/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+          <title>Events Tracker</title>
+          <meta name="viewport"content="initial-scale=1.0, width=device-width"/>
+          <meta name="description" content="Find all booming events here!" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
