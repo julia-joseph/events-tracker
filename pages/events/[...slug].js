e389b19566da4c17d4552b65009f024d36b4cba6
julia-joseph <jujoe726@gmail.com>
2021-08-01 13:41:42 +0530

    refactoring with useSWR()

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,16 +1,40 @@
+import { useEffect, useState } from 'react';
 import { useRouter } from "next/router";
+
+import useSWR from 'swr';
+
 import EventsList from "../../components/events/events-list";
 import { getFilteredEvents } from "../../dummy-data";
 import Alert from "../../ui/alert";
 
 const FilteredEventsPage = () => {
+  const [loadedEvents, setLoadedEvents] = useState();
+
   const router = useRouter();
+  const filter = router.query.slug;
+
+  const { data, error } = useSWR(
+    'https://next-learn-493fd-default-rtdb.firebaseio.com/events.json'
+  );
 
   console.log("[FilteredEventsPage.js] ", router.query);
 
-  const filter = router.query.slug;
+  useEffect(() => {
+    if (data) {
+      const events = [];
 
-  if (!filter) {
+      for (const key in data) {
+        events.push({
+          id: key,
+          ...data[key],
+        });
+      }
+
+      setLoadedEvents(events);
+    }
+  }, [data]);
+
+  if (!loadedEvents) {
     return <p>Loading... </p>;
   }
 
@@ -28,16 +52,27 @@ const FilteredEventsPage = () => {
     return <Alert><p>Invalid Filter!! Please check your values!</p></Alert>;
   }
 
-  const filteredEvents = getFilteredEvents({
-    year: year,
-    month: month,
+  if( error ) {
+    return <Alert><p>Error Loading Events!</p></Alert>
+  }
+
+  const filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year &&
+      eventDate.getMonth() === month - 1
+    );
   });
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return <Alert><p>No events scheduled!</p></Alert>
   }
 
-  return <EventsList items={filteredEvents}></EventsList>;
+  return (
+    <>
+      <EventsList items={filteredEvents}></EventsList>
+    </>
+  );
 };
 
 export default FilteredEventsPage;
