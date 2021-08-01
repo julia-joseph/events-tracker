c4efb6b39d1909adca1dab90bce4a672c1e48a6b
julia-joseph <jujoe726@gmail.com>
2021-08-01 13:43:08 +0530

    adding head to all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import EventsList from "../../components/events/events-list";
 import EventsSearch from "../../components/events/events-search";
 import { getEvents } from "../../utils/api-utils";
@@ -5,6 +7,13 @@ import { getEvents } from "../../utils/api-utils";
 const EventsPage = props => {
   return (
     <>
+      <Head>
+        <title>All Events</title>
+        <meta
+          name="description"
+          content="Find a bunch of events you've been waiting for"
+        />
+      </Head>
       <EventsSearch></EventsSearch>
       <EventsList items={props.events}></EventsList>
     </>
