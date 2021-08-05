e7acdf758a52dc024eef4606752ac19eb6dce9b4
Julia Joseph <juljoe726@gmail.com>
2021-08-05 09:13:16 +0530

    adding notification provider in app.js + formatting

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,24 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 import Layout from "../layout/layout";
+import { NotificationContextProvider } from "../store/notification-context";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
           <title>Events Tracker</title>
-          <meta name="viewport"content="initial-scale=1.0, width=device-width"/>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
           <meta name="description" content="Find all booming events here!" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
