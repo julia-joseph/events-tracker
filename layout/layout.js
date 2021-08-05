bc418340830d95ad4cccb84cf3cfaa6a6048a729
Julia Joseph <juljoe726@gmail.com>
2021-08-05 09:14:18 +0530

    adding notification component to layout

--- layout/layout.js
+++ layout/layout.js
@@ -1,10 +1,24 @@
+import { useContext } from "react";
+import NotificationContext from "../store/notification-context";
+import Notification from "../ui/notification";
 import Header from "./main-header";
 
 const Layout = (props) => {
+  const context = useContext(NotificationContext);
+
+  const notification = context.notification;
+
   return (
     <>
       <Header></Header>
       <main>{props.children}</main>
+      {notification && (
+        <Notification
+          title={notification.title}
+          message={notification.message}
+          status={notification.status}
+        />
+      )}
     </>
   );
 };
